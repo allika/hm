f85dd5583fa0230e7eb96b44338dd9b1676f61f3
Alla Lisitskaya <avl@dom1nga.4.office>
2012-08-29 16:28:30 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hm::Application.config.secret_token = '9feb347de5084a746c33c17173989ec10a5da4dbb1b835b5dc803c098c6a372002051d0e06f1591877d4352226891e122366418751106992e44ec7bc9d82aa5a'

01fe78e16a963319d76e57e3b33de78016e9a8e0
Alla Lisitskaya <avl@avl.4.office>
2012-09-05 15:56:54 +0300

    Changed root route direction.

--- config/routes.rb
+++ config/routes.rb
@@ -61,5 +61,5 @@ Hm::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  root :to => "home_page#show"
+  root :to => "home_page#index"
 end

2e5f23ecc83547e306b3f0d78a8147c0e466ff23
Alla Lisitskaya <a.lisitska@gmail.com>
2012-09-10 09:05:46 +0300

    Fixed double error messages during sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
 
   validates :email, :presence => true, :length => { :maximum => 60 },
-            :format => EMAIL_REGEX, :uniqueness => { :case_sensitive => false }
-  validates :password, :presence => true, :length => { :in => 6..35 }
+            :format => EMAIL_REGEX
+  validates :password, :presence => true
   validates :password_confirmation, :presence => true
 end
